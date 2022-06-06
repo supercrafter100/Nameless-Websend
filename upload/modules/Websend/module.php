51cb252ddc92352aa3785ce15c3ea1d45d237a50
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2022-06-06 15:01:31 +0200

    remove ancient code

--- upload/modules/Websend/module.php
+++ upload/modules/Websend/module.php
@@ -10,30 +10,31 @@
  *  Websend module for NamelessMC
  */
 
-class Websend_Module extends Module {
-	private $_language, $_websend_language, $_cache, $_endpoints;
+class Websend_Module extends Module
+{
+    private $_language, $_websend_language, $_cache, $_endpoints;
 
-	public function __construct($pages, $language, $websend_language, $cache, $endpoints){
-		$this->_language = $language;
-		$this->_websend_language = $websend_language;
-		$this->_cache = $cache;
+    public function __construct($pages, $language, $websend_language, $cache, $endpoints)
+    {
+        $this->_language = $language;
+        $this->_websend_language = $websend_language;
+        $this->_cache = $cache;
         $this->_endpoints = $endpoints;
 
-		$name = 'Websend';
-		$author = '<a href="https://github.com/supercrafter100/" target="_blank" rel="nofollow noopener">Supercrafter100</a>';
-		$module_version = '1.1.2';
-		$nameless_version = '2.0.0-pr13';
+        $name = 'Websend';
+        $author = '<a href="https://github.com/supercrafter100/" target="_blank" rel="nofollow noopener">Supercrafter100</a>';
+        $module_version = '1.1.2';
+        $nameless_version = '2.0.0-pr13';
 
-		parent::__construct($this, $name, $author, $module_version, $nameless_version);
+        parent::__construct($this, $name, $author, $module_version, $nameless_version);
 
-		// Define URLs which belong to this module
-		$pages->add('Websend', '/panel/websend', 'pages/panel/websend.php');
+        // Define URLs which belong to this module
+        $pages->add('Websend', '/panel/websend', 'pages/panel/websend.php');
         $pages->add('Websend', '/panel/websend/servers', 'pages/panel/websend_servers.php');
         $pages->add('Websend', '/panel/websend/hooks', 'pages/panel/websend_hooks.php');
         $pages->add('Websend', '/panel/websend/hooks/edit', 'pages/panel/websend_hooks_edit.php');
         $pages->add('Websend', '/panel/websend/settings', 'pages/panel/websend_settings.php');
         $pages->add('Websend', '/queries/console', 'queries/GetConsoleContent.php');
-        $pages->add('Websend', '/queries/console/clear', 'queries/ClearConsole.php');
 
         // Loading API endpoins
         $endpoints->loadEndpoints(join(DIRECTORY_SEPARATOR, array(ROOT_PATH, 'modules', 'Websend', 'includes', 'endpoints')), $endpoints);
@@ -43,22 +44,23 @@ class Websend_Module extends Module {
         if ($this->_cache->retrieve('installed')) {
             WSHook::registerEvents();
         };
-	}
+    }
 
-	public function onInstall() {
+    public function onInstall()
+    {
 
-		try {
-			$engine = Config::get('mysql/engine');
-			$charset = Config::get('mysql/charset');
-		} catch(Exception $e){
-			$engine = 'InnoDB';
-			$charset = 'utf8mb4';
-		}
+        try {
+            $engine = Config::get('mysql/engine');
+            $charset = Config::get('mysql/charset');
+        } catch (Exception $e) {
+            $engine = 'InnoDB';
+            $charset = 'utf8mb4';
+        }
 
-        if(!$engine || is_array($engine))
+        if (!$engine || is_array($engine))
             $engine = 'InnoDB';
 
-        if(!$charset || is_array($charset))
+        if (!$charset || is_array($charset))
             $charset = 'latin1';
 
         $db = DB::getInstance();
@@ -138,30 +140,34 @@ class Websend_Module extends Module {
         // Set installed
         $this->_cache->setCache('websend_module');
         $this->_cache->store('installed', true);
-	}
+    }
 
-	public function onUninstall() {
-		// Not implemented yet
-	}
+    public function onUninstall()
+    {
+        // Not implemented yet
+    }
 
-	public function onEnable() {
-		// Not necessary
-	}
+    public function onEnable()
+    {
+        // Not necessary
+    }
 
-	public function onDisable() {
-		// Not necessary
-	}
+    public function onDisable()
+    {
+        // Not necessary
+    }
 
-	public function onPageLoad(User $user, Pages $pages, Cache $cache, Smarty $smarty, $navs, Widgets $widgets, ?TemplateBase $template) {
+    public function onPageLoad(User $user, Pages $pages, Cache $cache, Smarty $smarty, $navs, Widgets $widgets, ?TemplateBase $template)
+    {
 
-		// Permissions
-		PermissionHandler::registerPermissions('Websend', [
+        // Permissions
+        PermissionHandler::registerPermissions('Websend', [
             'admincp.websend.console' => $this->_language->get('moderator', 'staff_cp') . ' &raquo; ' . $this->_websend_language->get('language', 'websend_console'),
-			'admincp.websend.events' => $this->_language->get('moderator', 'staff_cp') . ' &raquo; ' . $this->_websend_language->get('language', 'websend_events'),
+            'admincp.websend.events' => $this->_language->get('moderator', 'staff_cp') . ' &raquo; ' . $this->_websend_language->get('language', 'websend_events'),
             'admincp.websend.settings' => $this->_language->get('moderator', 'staff_cp') . ' &raquo; ' . $this->_websend_language->get('language', 'websend_settings'),
-		]);
+        ]);
 
-		if(defined('BACK_END')){
+        if (defined('BACK_END')) {
 
             if (
                 $user->hasPermission('admincp.websend.console') ||
@@ -196,10 +202,11 @@ class Websend_Module extends Module {
                     $navs[2]->add('websend_settings', $this->_websend_language->get('language', 'websend_settings'), URL::build('/panel/websend/settings'), 'top', null, ($order + 0.3), $settings_icon);
                 }
             }
-		}
-	}
+        }
+    }
 
-    public function getDebugInfo(): array {
+    public function getDebugInfo(): array
+    {
         return [];
     }
 }
